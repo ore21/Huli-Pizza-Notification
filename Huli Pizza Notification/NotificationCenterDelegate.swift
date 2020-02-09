29e5e53be91ace84e3904f0f88950ecf0bc77f2b
Brandon Gonzalez <bgonzalez526@students.mchenry.edu>
2020-02-09 12:06:45 -0600

    Initial commit

---
+++ Huli Pizza Notification/NotificationCenterDelegate.swift
@@ -0,0 +1,79 @@
+//
+//  NotificationCenterDelegate.swift
+//  HuliPizzaNotifications
+//
+//  Created by Steven Lipton on 10/9/18.
+//  Copyright © 2018 Steven Lipton. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class NotificationCenterDelegate: NSObject,UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert,.badge])
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let action = response.actionIdentifier
+        let request = response.notification.request
+        let content = request.content.mutableCopy() as! UNMutableNotificationContent
+        if action == "text.input"{
+            let textResponse = response as! UNTextInputNotificationResponse
+            let newContent = request.content.mutableCopy() as! UNMutableNotificationContent
+            newContent.subtitle = textResponse.userText
+            let request = UNNotificationRequest(identifier: request.identifier, content: newContent, trigger: request.trigger)
+            UNUserNotificationCenter.current().add(request) { (error) in
+                self.printError(error, location: "Text Input action")
+            }        }
+        
+        if action == "cancel"{
+            UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [request.identifier])
+        }
+        if action == "snooze"{
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+            let content = changePizzaNotification(content: request.content)
+            let request = UNNotificationRequest(identifier: request.identifier, content: content, trigger: trigger)
+            
+            UNUserNotificationCenter.current().add(request) { (error) in
+                self.printError(error, location: "Snooze Action")
+            }
+        }
+        if action == "next.step"{
+            guard var step = content.userInfo["step"] as? Int else {return}
+            step += 1
+            if step < pizzaSteps.count {
+                content.body = pizzaSteps[step]
+                content.userInfo["step"] = step
+                let request = UNNotificationRequest(identifier: request.identifier, content: content, trigger: request.trigger)
+                UNUserNotificationCenter.current().add(request) { (error) in
+                    self.printError(error, location: "Next Step action")
+                }
+            } else {
+            UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [request.identifier])
+            }
+        }
+        completionHandler()
+    }
+    
+    func changePizzaNotification(content oldContent:UNNotificationContent) -> UNMutableNotificationContent{
+        let content = oldContent.mutableCopy() as! UNMutableNotificationContent
+        let userInfo = content.userInfo as! [String:Any]
+        if let orders = userInfo["order"] as? [String]{
+            content.body = "You are going to love this:\n"
+            for item in orders{
+                content.body += surferBullet + item + "\n"
+            }
+        }
+        return content
+    }
+    
+    
+    //MARK: - Support Methods
+    let surferBullet = "🏄🏽‍♀️ "
+    // A function to print errors to the console
+    func printError(_ error:Error?,location:String){
+        if let error = error{
+            print("Error: \(error.localizedDescription) in \(location)")
+        }
+    }
+}
---
+++ Huli Pizza Notification/View Controllers/ManageNotificationsViewController.swift
@@ -0,0 +1,142 @@
+//
+//  ManageNotificationsViewController.swift
+//  HuliPizzaNotifications
+//
+//  Created by Steven Lipton on 9/27/18.
+//  Copyright © 2018 Steven Lipton. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class ManageNotificationsViewController: UIViewController{
+    
+    @IBOutlet weak var pendingNotificationButton: UIButton!
+    @IBOutlet weak var consoleView: UITextView!
+    
+    //let pizzaSteps = ["Make pizza", "Roll Dough", "Add Sauce", "Add Cheese", "Add Ingredients", "Bake", "Done"]
+    
+    
+    @IBAction func backButton(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func viewPendingNotifications(_ sender: UIButton) {
+        UNUserNotificationCenter.current().getPendingNotificationRequests { (requests) in
+            self.printRequest(count: requests.count, type: "pending")
+            for request in requests{
+                self.printConsoleView("\(request.identifier):\(request.content.body)\n")
+            }
+        }
+    }
+    
+    @IBAction func viewDeliveredNotifications(_ sender: UIButton) {
+        UNUserNotificationCenter.current().getDeliveredNotifications { (notifications) in
+            self.printRequest(count: notifications.count, type: "Delivered")
+            for notification in notifications{
+                self.printConsoleView("\(notification.request.identifier):\(notification.request.content.body)\n")
+            }
+        }
+    }
+    
+    @IBAction func removeAllNotifications(_ sender: UITapGestureRecognizer) {
+       UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+    }
+    
+    @IBAction func removeNotification(_ sender: UIButton) {
+        UNUserNotificationCenter.current().getPendingNotificationRequests { (requests) in
+            if let request = requests.first( where: {request -> Bool in request.trigger?.repeats == true}){
+                UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [request.identifier])
+            }
+        }
+    }
+    
+    @IBAction func nextPizzaStep(_ sender: UIButton) {
+        UNUserNotificationCenter.current().getPendingNotificationRequests { (requests) in
+            for request in requests{
+                if request.identifier.hasPrefix("message.pizza"){
+                    guard let content = self.updatePizzaContent(request:  request) else{
+                        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [request.identifier])
+                        return
+                    }
+                    self.addNotification(trigger: request.trigger, content: content, identifier: request.identifier)
+                    return
+                }
+            }
+        }
+    }
+    
+    func updatePizzaContent(request:UNNotificationRequest)-> UNMutableNotificationContent!{
+        if let stepNumber = request.content.userInfo["step"] as? Int{
+            let newStepNumber = (stepNumber + 1 )
+            let updatedContent = request.content.mutableCopy() as! UNMutableNotificationContent
+            if newStepNumber >= pizzaSteps.count {return nil}
+            if newStepNumber == pizzaSteps.count - 1 {
+                updatedContent.threadIdentifier = request.identifier
+                updatedContent.categoryIdentifier = "snooze.category"
+            }
+            updatedContent.body = pizzaSteps[newStepNumber]
+            updatedContent.userInfo["step"] = newStepNumber
+            return updatedContent
+        }
+        return request.content as? UNMutableNotificationContent
+    }
+    
+    
+    
+    //MARK: - Life Cycle
+    override func viewDidLayoutSubviews() {
+        // rouds to corners of the console view
+        consoleView.layer.cornerRadius = consoleView.frame.height * 0.05
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+     //MARK: - Support Methods
+    func printConsoleView(_ string:String){
+        DispatchQueue.main.async {
+            self.consoleView.text += string
+        }
+    }
+    func clearConsoleView(){
+        DispatchQueue.main.async {
+            self.consoleView.text = ""
+        }
+    }
+    
+    //a function to nicely print the date, count and type of notification.
+    func printRequest(count:Int, type:String){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .none
+        dateFormatter.timeStyle = .medium
+        let dateString = dateFormatter.string(from: Date())
+        var countString = String(format: "%i",count)
+        countString = dateString + "---->" + countString + " requests " + type + "\n"
+        clearConsoleView()
+        printConsoleView(countString)
+    }
+    
+    func notificationContent(title:String,body:String)->UNMutableNotificationContent{
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.userInfo = ["step":0]
+        return content
+    }
+    func addNotification(trigger:UNNotificationTrigger?,content:UNMutableNotificationContent,identifier:String){
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request) { (error) in
+            self.printError(error, location: "Add Request for Identifier:" + identifier)
+        }
+    }
+    
+    // A function to print errors to the console
+    func printError(_ error:Error?,location:String){
+        if let error = error{
+            print("Error: \(error.localizedDescription) in \(location)")
+        }
+    }
+}
